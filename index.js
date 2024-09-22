7102924a16460e16d0cff7705d4125c9163d5b77
“linhPham98” <linhthusinh98@gmail>
2024-09-22 21:48:39 +0900

    commit config BE

---
+++ index.js
@@ -0,0 +1,65 @@
+import express from "express";
+import { createClient } from "@supabase/supabase-js";
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+const port = 5000;
+
+const supabaseUrl = process.env.SUPBASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+const corsOptions = {
+  origin: ["https://linhthusinh.vercel.app", "http://localhost:5173"],
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+// get list data info from supabase
+app.get("/api/info", async (req, res) => {
+  try {
+    const { data, error } = await supabase.from("info").select("*");
+    if (error) {
+      return res.status(500).json({ error: error.message });
+    }
+
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// add new records to info
+app.post("/api/info", async (req, res) => {
+  const { id, namelogin, nameshow, email, avatar, department, password, job } =
+    req.body;
+
+  const { data, error } = await supabase
+    .from("info") // Tên bảng của bạn
+    .insert([
+      { id, namelogin, nameshow, email, avatar, department, password, job },
+    ]);
+
+  if (error) {
+    return res.status(400).json({ error: error.message });
+  }
+
+  res.status(200).json(data);
+});
+
+app.get("/api/chat", async (req, res) => {
+  try {
+    const { data, error } = await supabase.from("chat").select("*");
+    if (error) {
+      return res.status(500).json({ error: error.message });
+    }
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`API is running on http://localhost:${port}`);
+});
