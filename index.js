525f54ca833b30a68cf3a6141af21dcc8ca6e4cf
“linhPham98” <linhthusinh98@gmail>
2024-10-09 00:04:31 +0900

    commit

--- index.js
+++ index.js
@@ -335,6 +335,10 @@ io.on("connection", (socket) => {
   });
 });
 
+io.connect(url, {
+  "transports": ['websocket']
+})
+
 httpServer.listen(port, () => {
   console.log(`API is running on http://localhost:${port}`);
 });
